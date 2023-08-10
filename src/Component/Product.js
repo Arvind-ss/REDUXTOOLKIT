ddda3302bdd94c4faddfba4a989e2cfa5a9adcce
Arvind-ss <128106810+Arvind-ss@users.noreply.github.com>
2023-08-10 07:42:11 +0530

    Update Product.js

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,38 +1,56 @@
-import React from 'react'
-import { useEffect,useState } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-import './Product.css';
+import "./Product.css";
 
 const Product = () => {
-    const url="https://fakestoreapi.com/products"
-    const[product,setProducts]=useState([])
-    const fetchData=async()=> {
-        try {
-            const response=await axios(url)            
-            const data=response.data;
-            setProducts(data)           
-       
-        } catch (error) {
-            console.log(error.message);
-        }
+  const url = "https://fakestoreapi.com/products";
+  const [product, setProducts] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      const data = response.data;
+      setProducts(data);
+    } catch (error) {
+      console.log(error.message);
     }
-    useEffect(()=>{fetchData()},[]
-        )
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const cards=product.map(product=>(
+    <div className="col-md-3" style={{marginBottom:"10px"}}>
+
+    <Card key={product.id} className="h-100">
+      <div className="text-center">
+      <Card.Img variant="top" src={product.image} style={{width:"100px",height:"130px"}} />
+      </div>
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+          INR:{product.price}
+        </Card.Text>       
+      </Card.Body>
+      <Card.Footer>      
+        <Button variant="primary">Add To Cart</Button>
+      </Card.Footer>
+    </Card>
+    
+    </div>
+  ))
   return (
     <>
-    <div className='parent' >
-        <h1 className='header' md="auto">Product DashBoard</h1>
-        <div>
-        {JSON.stringify(product)}
-        </div>
-        
-    </div>
-    
-    
-        
+      <div className="parent">
+        <h1 className="header" md="auto">
+          Product DashBoard
+        </h1>
+        <div className="row">{cards}</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
