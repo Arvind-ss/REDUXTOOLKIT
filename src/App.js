9003663120ce3ad6d862ed20e9cf34ddeef5666a
Arvind-ss <128106810+Arvind-ss@users.noreply.github.com>
2023-08-10 08:00:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Product from './Component/Product';
-
+import { createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom';
 function App() {
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route>
+        <Route></Route>
+    </Route>
+  ))
   return (
-    <>
-      <Product/>
-    </>
+    <div className="App">
+     <Product/>
+    </div>
   );
 }
 
