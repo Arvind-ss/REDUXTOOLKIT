0078f1d63b3eeab7012fffc57b55eb6f9bb9447c
Arvind-ss <aravindss.work@gmail.com>
2023-08-11 15:49:02 +0530

    redux cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Product from './Component/Product';
-<<<<<<< HEAD
+
 import { createBrowserRouter,createRoutesFromElements,RouterProvider,Route } from 'react-router-dom';
 import Dashboard from './Component/Dashboard';
 import Cart from './Component/Cart';
 import RootLayout from './Component/RootLayout';
+
 function App() {
   const router=createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<RootLayout/>}>
@@ -13,22 +13,12 @@ function App() {
         <Route path='/cart' element={<Cart/>}></Route>
     </Route>
   ))  
+  
+
   return (
     <div className="App">
       <RouterProvider router={router}/>
-     {/* <Product/> */}
-=======
-import { createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom';
-function App() {
-  const router=createBrowserRouter(createRoutesFromElements(
-    <Route>
-        <Route></Route>
-    </Route>
-  ))
-  return (
-    <div className="App">
-     <Product/>
->>>>>>> 9003663120ce3ad6d862ed20e9cf34ddeef5666a
+    
     </div>
   );
 }
--- src/Component/Product.js
+++ src/Component/Product.js
@@ -3,78 +3,67 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-<<<<<<< HEAD
 import { useDispatch } from "react-redux";
 import { add } from "../Store/CartSlice";
 import "./Product.css";
 
-const Product = () => {
-    //dispatch a add action
-    const dispatch=useDispatch()
-=======
 
-import "./Product.css";
-
-const Product = () => {
->>>>>>> 9003663120ce3ad6d862ed20e9cf34ddeef5666a
-  const url = "https://fakestoreapi.com/products";
-  const [product, setProducts] = useState([]);
-  const fetchData = async () => {
-    try {
-      const response = await axios(url);
-      const data = response.data;
-      setProducts(data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-<<<<<<< HEAD
-  const addToCart=(product)=>{
-    dispatch(add(product))
-  }
-=======
->>>>>>> 9003663120ce3ad6d862ed20e9cf34ddeef5666a
-  const cards=product.map(product=>(
-    <div className="col-md-3" style={{marginBottom:"10px"}}>
-
-    <Card key={product.id} className="h-100">
-      <div className="text-center">
-      <Card.Img variant="top" src={product.image} style={{width:"100px",height:"130px"}} />
-      </div>
-      <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
-        <Card.Text>
-          INR:{product.price}
-        </Card.Text>       
-      </Card.Body>
-      <Card.Footer>      
-<<<<<<< HEAD
-        <Button variant="primary" onClick={()=>addToCart(product)}>Add To Cart</Button>
-=======
-        <Button variant="primary">Add To Cart</Button>
->>>>>>> 9003663120ce3ad6d862ed20e9cf34ddeef5666a
-      </Card.Footer>
-    </Card>
+  const Product = () => {
     
-    </div>
-  ))
-  return (
-    <>
-      <div className="parent">
-        <h1 className="header" md="auto">
-          Product DashBoard
-        </h1>
-        <div className="row">{cards}</div>
-      </div>
-    </>
-  );
-};
+  //dispatch a add action
+  const dispatch = useDispatch();
+    const url = "https://fakestoreapi.com/products";
+    const [products, setProducts] = useState([]);
+    const fetchData = async () => {
+      try {
+        const response = await axios(url);
+        const data = response.data;
+        setProducts(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    useEffect(() => {
+      fetchData();
+    }, []);
 
-<<<<<<< HEAD
+    const addToCart = (product) => {
+      dispatch(add(product));
+    };
+
+    const cards = products.map((product) => (
+      <div className="col-md-3" style={{ marginBottom: "10px" }}>
+        <Card key={product.id} className="h-100">
+          <div className="text-center">
+            <Card.Img
+              variant="top"
+              src={product.image}
+              style={{ width: "100px", height: "130px" }}
+            />
+          </div>
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>INR:{product.price}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="primary" onClick={() => addToCart(product)}>
+              Add To Cart
+            </Button>
+
+          </Card.Footer>
+        </Card>
+      </div>
+    ));
+    return (
+      <>
+        <div className="parent">
+          <h1 className="header" md="auto">
+            Product DashBoard
+          </h1>
+          <div className="row">{cards}</div>
+        </div>
+      </>
+    );
+  };
+
 export default Product;
-=======
-export default Product;
->>>>>>> 9003663120ce3ad6d862ed20e9cf34ddeef5666a
